12a46b71d4b372ec951b1c1b109dc4205435ba21
R-Currie <currierobertm@gmail.com>
2019-12-04 14:04:25 -0600

    Notecard component

---
+++ src/components/NoteCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class NoteCard extends React.Component {
+
+    render(){
+        return(
+            <div>
+                Note Card
+            </div>
+        )
+    }
+}
+
+export default NoteCard;
